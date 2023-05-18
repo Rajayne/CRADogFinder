f13e4ddd0be0ee136919d9ba424d6d7982515b74
Rajayne <ryann.j.nealey@gmail.com>
2023-05-18 12:41:36 -0700

    Create ColorForm component and style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import DogList from "./DogFinder/DogList";
 import DogDetails from "./DogFinder/DogDetails";
 import ColorList from "./ColorFactory/ColorList";
+import ColorForm from "./ColorFactory/ColorForm";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="App-content">
         <Routes>
           <Route exact="true" path="/colors" element={<ColorList />} />
+          <Route exact="true" path="/colors/new" element={<ColorForm />} />
           <Route exact="true" path="/dogs" element={<DogList />} />
           <Route exact="true" path="/dogs/:name" element={<DogDetails />} />
           <Route path="*" element={<Navigate to="/dogs" />} />
---
+++ src/ColorFactory/ColorForm.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./ColorForm.css";
+
+const ColorForm = () => {
+  return (
+    <form className="ColorForm">
+      <h3>Add Color Form</h3>
+      <div className="ColorForm-name">
+        <label className="ColorForm-label" htmlFor="name">
+          Color Name:
+        </label>
+        <input className="ColorForm-input" type="text" name="name"></input>
+      </div>
+      <div className="ColorForm-hex">
+        <label className="ColorForm-label" htmlFor="hex">
+          Color Value:
+        </label>
+        <input className="ColorForm-input" type="color" name="hex"></input>
+      </div>
+      <button>Submit Color!</button>
+    </form>
+  );
+};
+
+export default ColorForm;
