9586bdf9d432fc013a998fa8e3e04899114c545e
Rajayne <ryann.j.nealey@gmail.com>
2023-05-18 11:47:40 -0700

    If dog not found, redirect to '/dogs' using useNavigate Redirect depreciated

--- src/DogFinder/DogDetails.js
+++ src/DogFinder/DogDetails.js
@@ -1,13 +1,23 @@
-import React from "react";
-import { redirect, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import dogs from "./dogs";
 import "./DogDetails.css";
 
 const DogDetails = () => {
+  const navigate = useNavigate();
   const { name } = useParams();
-  let dog = dogs.find((dog) => dog.name === name);
+  const [dog, setDog] = useState(null);
 
-  if (dog) {
+  useEffect(() => {
+    const fetchDog = async () => {
+      setDog(dogs.find((dog) => dog.name === name));
+    };
+    fetchDog();
+  }, [name]);
+
+  if (!dog) {
+    navigate("/dogs");
+  } else {
     return (
       <div className="DogDetails">
         <h1 className="DogDetails-title">{dog.name} Details</h1>
@@ -20,8 +30,6 @@ const DogDetails = () => {
         </ul>
       </div>
     );
-  } else {
-    return redirect("/dogs");
   }
 };
 
