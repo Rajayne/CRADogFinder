37feed8d1a1311ce1b6f2c59f824467fd698eae0
Rajayne <ryann.j.nealey@gmail.com>
2023-05-18 13:48:56 -0700

    Add colors to localStorage

--- src/ColorFactory/ColorList.js
+++ src/ColorFactory/ColorList.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import ColorNav from "./ColorNav";
 import "./ColorList.css";
+import colors from "./colors";
 
 const ColorList = () => {
+  const [colorList, setColorList] = useState(colors);
+
+  useEffect(() => {
+    setColorList(colors);
+    localStorage.setItem("colors", JSON.stringify(colorList));
+  }, [colorList]);
+
   return (
     <div className="ColorList">
       <div className="ColorList-header">
